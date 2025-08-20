bc463e8984dd497214552e83368b7bb4f9fd9438
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-20 21:29:46 +0700

    first commit

---
+++ src/main/java/com/phegon/PhegonAirline/PhegonAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.phegon.PhegonAirline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhegonAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhegonAirlineApplication.class, args);
+	}
+
+}
