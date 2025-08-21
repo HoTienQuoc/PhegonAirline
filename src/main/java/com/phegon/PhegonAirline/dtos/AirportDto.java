9cca60584a64e06416fda0a4936f0623c668a7ae
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-21 19:09:57 +0700

    Enhance AirportDto with validation annotations and Lombok for improved data handling

--- src/main/java/com/phegon/PhegonAirline/dtos/AirportDto.java
+++ src/main/java/com/phegon/PhegonAirline/dtos/AirportDto.java
@@ -1,5 +1,29 @@
 package com.phegon.PhegonAirline.dtos;
 
-public class AirportDto {
+import com.phegon.PhegonAirline.enums.City;
+import com.phegon.PhegonAirline.enums.Country;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AirportDto {
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotEmpty(message = "City is required")
+    private City city;
+
+    @NotEmpty(message = "Country is required")
+    private Country country;
+
+    @NotBlank(message = "IataCode is required")
+    private String iataCode;
 }
