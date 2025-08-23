49ce5c351a2751cb75a71b620cf02354db0f2c7a
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-23 21:52:49 +0700

    Remove unused imports from PhegonAirlineApplication for cleaner code and improved maintainability.

--- src/main/java/com/phegon/PhegonAirline/PhegonAirlineApplication.java
+++ src/main/java/com/phegon/PhegonAirline/PhegonAirlineApplication.java
@@ -1,16 +1,11 @@
 package com.phegon.PhegonAirline;
 
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
-import jakarta.mail.internet.MimeMessage;
 
 @SpringBootApplication
 public class PhegonAirlineApplication {
